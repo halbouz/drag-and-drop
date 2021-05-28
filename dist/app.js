8da0ac8e981f9028baf37e23db51179c6a118997
ganer <ganeralkidz@gmail.com>
2021-05-27 20:00:40 -0400

    added project lists

--- dist/app.js
+++ dist/app.js
@@ -5,7 +5,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-//This decorator allows us to autobind our class's element when we call functions
+//This decorator autobinds our class's element when we call functions
 function AutoBind(_, __, descriptor) {
     const originalMethod = descriptor.value;
     const adjDescriptor = {
@@ -13,41 +13,94 @@ function AutoBind(_, __, descriptor) {
         get() {
             const boundFn = originalMethod.bind(this);
             return boundFn;
-        }
+        },
     };
     return adjDescriptor;
 }
+//This function checks if a string respects length constraints
+function validateString(input, [minLength, maxLength]) {
+    return input.length >= minLength && input.length <= maxLength;
+}
+//ProjectInput class
 class ProjectInput {
     constructor() {
         //This represents the template for the form that we want to render to the DOM
-        this.templateElement = document.getElementById('project-input');
-        //This represents the div in which we want to render the templates
-        this.hostElement = document.getElementById('app');
+        this.templateElement = document.getElementById("project-input");
+        //This represents the div in which we want to render the template
+        this.hostElement = document.getElementById("app");
         //We import the content of the template and assign it to our form element
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
-        this.element.id = 'user-input';
+        this.element.id = "user-input";
         //We insert our form element into our div
-        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+        this.hostElement.insertAdjacentElement("afterbegin", this.element);
         //This represents the title input
-        this.titleInputElement = this.element.querySelector('#title');
+        this.titleInputElement = this.element.querySelector("#title");
         //This represents the description input
-        this.descriptionInputElement = this.element.querySelector('#description');
+        this.descriptionInputElement = this.element.querySelector("#description");
         //This represents the people input
-        this.peopleInputElement = this.element.querySelector('#people');
-        this.configure();
+        this.peopleInputElement = this.element.querySelector("#people");
+        //We add an event listener that handles submitting the form
+        this.element.addEventListener("submit", this.handleSubmit);
+    }
+    //This function gathers the input in the form and handles it
+    gatherUserInput() {
+        const title = this.titleInputElement.value;
+        const description = this.descriptionInputElement.value;
+        const people = this.peopleInputElement.value;
+        if (validateString(title, [5, 40]) &&
+            validateString(description, [30, 200]) &&
+            people.length !== 0) {
+            this.clearInputs();
+            return [title, description, +people];
+        }
+        else {
+            alert("Invalid Input, please try again");
+            return;
+        }
+    }
+    //This clears the inputs once we submit the form
+    clearInputs() {
+        this.titleInputElement.value = "";
+        this.descriptionInputElement.value = "";
+        this.peopleInputElement.value = "";
     }
     handleSubmit(event) {
         event.preventDefault();
-        console.log(this.titleInputElement.value);
-    }
-    configure() {
-        this.element.addEventListener('submit', this.handleSubmit);
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, desc, people] = userInput;
+            console.log(title, desc, people);
+        }
     }
 }
 __decorate([
     AutoBind
 ], ProjectInput.prototype, "handleSubmit", null);
+//ProjectList Class
+class ProjectList {
+    constructor(type) {
+        this.type = type;
+        //This represents the template for the section we want to render to the DOM
+        this.templateElement = document.getElementById('project-list');
+        //This represents the div in which we want to render the template
+        this.hostElement = document.getElementById('app');
+        //We import the content of the template and assign it to our section element
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.element.id = `${this.type}-projects`;
+        //We insert our section element into our div
+        this.hostElement.insertAdjacentElement("beforeend", this.element);
+        this.renderContent();
+    }
+    renderContent() {
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector('ul').id = listId;
+        this.element.querySelector('h2').textContent = this.type.toUpperCase() + ' PROJECTS';
+    }
+}
 //Instantiating our class
 const projInput = new ProjectInput();
+const activePrjList = new ProjectList('active');
+const finishedPrjList = new ProjectList('finished');
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,62 +1,141 @@
-//This decorator allows us to autobind our class's element when we call functions
-function AutoBind(_: any, __: string, descriptor: PropertyDescriptor){
-    const originalMethod = descriptor.value;
-    const adjDescriptor: PropertyDescriptor = {
-        configurable: true,
-        get() {
-            const boundFn = originalMethod.bind(this)
-            return boundFn;
-        }
-    }
-    return adjDescriptor;
+//This decorator autobinds our class's element when we call functions
+function AutoBind(_: any, __: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
 }
 
+//This function checks if a string respects length constraints
+function validateString(
+  input: string,
+  [minLength, maxLength]: [number, number]
+) {
+  return input.length >= minLength && input.length <= maxLength;
+}
 
+//ProjectInput class
 class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
+  constructor() {
+    //This represents the template for the form that we want to render to the DOM
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+
+    //This represents the div in which we want to render the template
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    //We import the content of the template and assign it to our form element
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+
+    //We insert our form element into our div
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+
+    //This represents the title input
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+
+    //This represents the description input
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+
+    //This represents the people input
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    //We add an event listener that handles submitting the form
+    this.element.addEventListener("submit", this.handleSubmit);
+  }
+
+  //This function gathers the input in the form and handles it
+  private gatherUserInput(): [string, string, number] | void {
+    const title = this.titleInputElement.value;
+    const description = this.descriptionInputElement.value;
+    const people = this.peopleInputElement.value;
+
+    if (
+      validateString(title, [5, 40]) &&
+      validateString(description, [30, 200]) &&
+      people.length !== 0
+    ) {
+      this.clearInputs();
+      return [title, description, +people];
+    } else {
+      alert("Invalid Input, please try again");
+      return;
+    }
+  }
+
+  //This clears the inputs once we submit the form
+  private clearInputs() {
+    this.titleInputElement.value = "";
+    this.descriptionInputElement.value = "";
+    this.peopleInputElement.value = "";
+  }
+
+  @AutoBind
+  private handleSubmit(event: Event) {
+    event.preventDefault();
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.log(title, desc, people);
+    }
+  }
+}
+
+//ProjectList Class
+class ProjectList {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
-    element: HTMLFormElement;
-    titleInputElement: HTMLInputElement;
-    descriptionInputElement: HTMLInputElement;
-    peopleInputElement: HTMLInputElement;
+    element: HTMLElement;
 
-    constructor() {
-        //This represents the template for the form that we want to render to the DOM
-        this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
-        //This represents the div in which we want to render the templates
-        this.hostElement = document.getElementById('app')! as HTMLDivElement;
-        
-        //We import the content of the template and assign it to our form element
-        const importedNode = document.importNode(this.templateElement.content, true);
-        this.element = importedNode.firstElementChild as HTMLFormElement;
-        this.element.id = 'user-input'
+    constructor(private type: 'active' | 'finished') {
+        //This represents the template for the section we want to render to the DOM
+        this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
 
-        //We insert our form element into our div
-        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+        //This represents the div in which we want to render the template
+        this.hostElement = document.getElementById('app')! as HTMLDivElement
 
-        //This represents the title input
-        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+        //We import the content of the template and assign it to our section element
+        const importedNode = document.importNode(this.templateElement.content, true)
+        this.element = importedNode.firstElementChild as HTMLElement;
+        this.element.id = `${this.type}-projects`
 
-        //This represents the description input
-        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
-
-        //This represents the people input
-        this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
-        
-        this.configure();
+        //We insert our section element into our div
+        this.hostElement.insertAdjacentElement("beforeend", this.element);
+        this.renderContent()
     }
 
+    private renderContent() {
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector('ul')!.id = listId;
+        this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS'
+    }   
 
-    @AutoBind
-    private handleSubmit(event: Event) {
-        event.preventDefault();
-        console.log(this.titleInputElement.value);
-    }
-
-    private configure() {
-        this.element.addEventListener('submit', this.handleSubmit);
-    }
 }
 
 //Instantiating our class
 const projInput = new ProjectInput();
+const activePrjList = new ProjectList('active');
+const finishedPrjList = new ProjectList('finished');
